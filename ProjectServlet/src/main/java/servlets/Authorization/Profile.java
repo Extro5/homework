89d97273f20ca10769d5cd9ff2328be3a0a853cb
Extro5 <cool.extro5@yandex.ru>
2015-10-20 15:41:31 +0300

    update jsp profile

--- ProjectServlet/src/main/java/servlets/Authorization/Profile.java
+++ ProjectServlet/src/main/java/servlets/Authorization/Profile.java
@@ -7,7 +7,6 @@ import exceptions.DbException;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import static repositories.EnterForm.UserRep.searchName;
 
@@ -21,27 +20,12 @@ public class Profile extends HttpServlet {
 
         HttpSession session = req.getSession();
         Cookie[] cookies = req.getCookies();
-        PrintWriter out = resp.getWriter();
-        PrintWriter writer = resp.getWriter();
-        String htmlResponse = "<html>";
         for (Cookie cookie : cookies) {
             if (cookie.getValue().equals(session.getAttribute("cookie"))) {
-               /* htmlResponse += "<head>\n" +
-                        "  <meta charset=\"utf-8\">\n" +
-                        "    <title>Users</title>\n" +
-                        "   </head>";
-                htmlResponse += "<body>";*/
-
-
                 try {
                     if (searchName(cookie.getValue()) != null) {
                         User user = searchName(cookie.getValue());
-                        session.setAttribute("Username", req.getParameter("Username"));
-                        /*
                         assert user != null;
-                        String Username = user.getUsername();
-                        //getServletContext().getRequestDispatcher("/WEB-INF/Authorization/Profile.jsp").forward(req, resp);*/
-
                         String lol1;
                         if (user.getSex()) {
                             lol1 = "male";
@@ -54,105 +38,23 @@ public class Profile extends HttpServlet {
                         } else {
                             lol2 = "not agree";
                         }
-                        /*
-                        out.println("Username: " + user.getUsername());
-                        out.println("Email: " + user.getEmail());
-                        out.println("Password: " + user.getPassword());
-                        out.println("Sex: " + lol1);
-                        out.println("Subscribe: " + lol2);
-                        out.println("Comment " + user.getComment());*/
-                        htmlResponse += "<head>\n" +
-                                "  <meta charset=\"utf-8\">\n" +
-                                "    <title>Users</title>\n" +
-                                "   </head>";
-                        htmlResponse += "<body>";
-                        htmlResponse += "<div class=\"container\">";
-                        htmlResponse += "<div class=\"card\">";
-                        htmlResponse += "<h2>"+user.getUsername()+"</h2>";
-                        htmlResponse += "<p>"+"email: "+user.getEmail()+"</p>";
-                        htmlResponse += "<p>"+"Sex: "+lol1+"</p>";
-                        htmlResponse += "<p>"+"Subscribe: "+lol2+"</p>";
-                        htmlResponse += "<p>"+"Comment: "+user.getComment()+"</p>";
-                        htmlResponse +="</div>";
-                        htmlResponse += "</div>";
-                        htmlResponse += " <form name=\"loginForm\" method=\"get\" action=\"/logout\">\n" +
-                                "            <p><input type=\"submit\" value=\"logout\" /></p>\n" +
-                                "        </form>";
-                        htmlResponse += "</body>";
 
-                        htmlResponse += "</html>";
-                        writer.println(htmlResponse);
-                        out.close();
+                        req.setAttribute("Username", user.getUsername());
+                        req.setAttribute("Email", user.getEmail());
+                        req.setAttribute("Password", user.getPassword());
+                        req.setAttribute("Sex", lol1);
+                        req.setAttribute("Subscribe", lol2);
+                        req.setAttribute("Comment", user.getComment());
+
+                        getServletContext().getRequestDispatcher("/WEB-INF/Authorization/Profile.jsp").forward(req, resp);
+
                         return;
                     }
                 } catch (DbException e) {
                     e.printStackTrace();
                 }
-
-
-
-
             }
         }
         resp.sendRedirect("/login");
     }
 }
-
-      /*  _______________________________________________________________________________________*/
-
-/*
-        HttpSession session = req.getSession();
-        PrintWriter out = resp.getWriter();
-        if (session.getAttribute("user") == null) {
-            resp.sendRedirect("/login");
-        }
-        PrintWriter writer = resp.getWriter();
-        String htmlResponse = "<html>";
-
-        htmlResponse +="<head>\n" +
-                "  <meta charset=\"utf-8\">\n" +
-                "    <title>Users</title>\n" +
-                "   </head>";
-        htmlResponse += "<body>";
-        try {
-            String email = (String) session.getAttribute("user");
-            if (searchEmail(email) != null) {
-                User user = searchEmail(email);
-                String lol1;
-                assert user != null;
-                if (user.getSex()) {
-                    lol1 = "male";
-                } else {
-                    lol1 = "female";
-                }
-                String lol2;
-                if (user.getRadio()) {
-                    lol2 = "agree";
-                } else {
-                    lol2 = "not agree";
-                }
-                htmlResponse += "<div class=\"container\">";
-                htmlResponse += "<div class=\"card\">";
-                htmlResponse += "<h2>"+user.getUsername()+"</h2>";
-                htmlResponse += "<p>"+"email:"+user.getEmail()+"</p>";
-                htmlResponse += "<p>"+lol1+"</p>";
-                htmlResponse += "<p>"+"newsletter:"+lol2+"</p>";
-                htmlResponse += "<p>"+"Comment:<br>"+user.getComment()+"</p>";
-                htmlResponse +="</div>";
-                htmlResponse += "</div>";
-                htmlResponse += " <form name=\"loginForm\" method=\"get\" action=\"/logout\">\n" +
-                        "            <p><input type=\"submit\" value=\"exit\" /></p>\n" +
-                        "        </form>";
-
-
-            }
-        } catch (DbException e) {
-            htmlResponse += "<h1>Error!!!</h1>";
-        }
-        htmlResponse += "</body>";
-
-        htmlResponse += "</html>";
-        writer.println(htmlResponse);
-        out.close();
-    }
-}*/
--- ProjectServlet/target/projectservlet-1.0-SNAPSHOT/WEB-INF/Authorization/Profile.jsp
+++ ProjectServlet/target/projectservlet-1.0-SNAPSHOT/WEB-INF/Authorization/Profile.jsp
@@ -1,22 +1,29 @@
 <%@ taglib prefix="fmt" uri="http://java.sun.com/jstl/fmt" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" pageEncoding="UTF-8" %>
+
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
 <html>
 <head>
-  <title>Profile</title>
+    <meta http-equip="Content-Type" content="text/html; charset= UTF-8">
+    <title>Profile</title>
+
+
 </head>
 <body>
-<fmt:requestEncoding value="windows-1251"></fmt:requestEncoding>
-<h2>Welcome <span style="font-size: x-large; ">Приветствую &nbsp; ${User} </span></h2>
-<%=session.getAttribute("Username")%>
-<%=request.getParameter("User")%>
-<%=request.getParameter("Password")%>
-<%=request.getParameter("Email")%>
 
-
+<fmt:requestEncoding value="UTF-8"/>
+<h2>Welcome <span style="font-size: x-large; "> &nbsp; ${Username}  </span></h2>
+Email: ${Email} <br>
+Password: ${Password}<br>
+Sex: ${Sex}<br>
+Subscribe: ${lol1}<br>
+Comment: ${Comment}<br>
 
 
 <form action="/logout" method="get">
-  <input type="submit" value="quit"/>
+    <input type="submit" value="quit"/>
 </form>
 </body>
 </html>
