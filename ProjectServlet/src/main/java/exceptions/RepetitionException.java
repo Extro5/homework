4604972842aa3f2fb159eb2a561e9b6d20f5c393
Extro5 <cool.extro5@yandex.ru>
2015-11-01 16:04:01 +0300

    add ignore file

--- ProjectServlet/src/main/java/exceptions/RepetitionException.java
+++
@@ -1,7 +0,0 @@
-package exceptions;
-
-/**
- * Created by coole on 13.10.2015.
- */
-public class RepetitionException extends Exception{
-}
--- ProjectServlet/src/main/java/exceptions/UsernameException.java
+++
@@ -1,8 +0,0 @@
-package exceptions;
-
-
-
-public class UsernameException extends Exception  {
-
-    
-}
